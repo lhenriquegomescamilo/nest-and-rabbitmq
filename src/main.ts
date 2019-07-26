78f3b77b7cda8869bab3e73276eeb671b48cffb9
lcamilo <lhenriquegomescamilo@gmail.com>
2019-07-25 23:02:35 -0300

    Creating the hybrid application because
    RabbitMQ is supported in nestjs as a microservice.
    If we  want your application to support both http requests and a message broker,
    we can create a hybrid application.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/common/enums/transport.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://localhost:5672`],
+      queue: 'cats_queue',
+      queueOptions: { durable: false },
+    },
+  });
+  await app.startAllMicroservicesAsync();
   await app.listen(3000);
 }
+
 bootstrap();
