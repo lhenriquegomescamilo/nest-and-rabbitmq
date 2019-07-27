78a8ceb55d0b48d466be29c85b47b052d4ad6257
lcamilo <lhenriquegomescamilo@gmail.com>
2019-07-27 18:27:47 -0300

    Creating the  worker

---
+++ src/rabbit-mq/rabbit-mq-worker.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger } from '@nestjs/common';
+import * as ampq from 'amqplib';
+
+@Injectable()
+export class RabbitMqWorkerService {
+
+  private readonly logger = new Logger('RabbitMqWorkerService');
+
+  private static readonly assertQueueOptions = { durable: true };
+  private static readonly consumeQueueOptions = { noAck: false };
+
+  constructor() {
+    this.listemToQueue();
+  }
+
+  listemToQueue() {
+    return ampq
+      .connect(process.env.RABBITMQ_CONNECTION_URL)
+      .then(connection => connection.createChannel())
+      .then(channel => this.assertAndConsumeQueue(channel));
+  }
+
+  private assertAndConsumeQueue(channel) {
+    this.logger.log('Worker is running! Waiting for new messages...');
+    // @ts-ignore
+    const ackMessage = (message: any) => Promise.resolve(message)
+      .then((msg: any) => {
+        this.logger.log(`THE MESSSAGE IS ${msg.content.toString()}`);
+        return message;
+      })
+      .then((msg: any) => channel.ack(message));
+
+    return channel.assertQueue('worker_queue', RabbitMqWorkerService.assertQueueOptions)
+      .then(() => channel.prefetch(1))
+      .then(() => channel.consume('worker_queue', ackMessage, RabbitMqWorkerService.consumeQueueOptions));
+  }
+
+}
